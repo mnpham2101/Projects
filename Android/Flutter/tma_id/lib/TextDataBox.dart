ac9b123d1c2b4555b09d8ea1c6c5dcb5da17d2f8
Freewind <mnpham1986@gmail.com>
2024-08-26 22:57:58 +0700

    .

---
+++ Android/Flutter/tma_id/lib/TextDataBox.dart
@@ -0,0 +1,30 @@
+class TextDataBox extends StatefulWidget {
+  const TextDataBox({
+    super.key,
+    this.color = const Color(0xFFFFE306),
+    this.child,
+  });
+
+  final Color color;
+  final Widget? child;
+
+  @override
+  State<Bird> createState() => _TextDataBoxState();
+}
+
+class _TextDataBoxState extends State<TextDataBox> {
+  double _size = 1.0;
+
+  void grow() {
+    setState(() { _size += 0.1; });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: widget.color,
+      transform: Matrix4.diagonal3Values(_size, _size, 1.0),
+      child: widget.child,
+    );
+  }
+}
